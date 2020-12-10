03afeee1b2fb092b487b260f5764ae03ca3d7c08
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-10 12:10:21 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,100 +1,31 @@
 //const { Message } = require("discord.js");
 const Discord = require('discord.js');
-const paginationEmbed = require('discord.js-pagination');
 module.exports.run = async(bot, message , args) => {
+
+
+    const help = new Discord.MessageEmbed()
+    .setColor("GREEN")
+    .setTitle("Raju Commands")
+    .setDescription(`
+       • a.help → This panel
+    • a.ping →  Check  whether I am alive or dead
+    • a.rip → Announce ur funeral
+    • a.a → Your Avatar
+    • a.avenger → makes u avenger
+    • a.calc → Solves ur sums , subtraction , multiplication , division
     
-
-    const help1 = new Discord.MessageEmbed().setColor("GREEN").setAuthor('Raju Commands').setTitle("Info").setDescription(` • a.help → This panel
-• a.ping →  Check  whether I am alive or dead
-
-• a.a → Your Avatar
-
-• a.stats
-
-• a.aboutme
-
-
-• a.support
-
-   `);
-
-   const help2 = new Discord.MessageEmbed().setColor("GREEN").setAuthor('Raju Commands').setTitle("Fun").setDescription(`• a.rip
-
-   • a.ascii 
-   
-   • a.avenger
-   
-   • a.8ball  
+    • a.support → Support me!
+    • a.meme -> a random meme !
     
-   • a.simp
+    • a.8ball -> Have a Question? ask me !
+    • a.neko -> Random cat like weeb image :)
+    • a.kitty -> Random kitty images :)
+   `)
 
-   • a.meme
-   
-   `);
-   
-   
-   const help3 = new Discord.MessageEmbed().setColor("GREEN").setAuthor('Raju Commands').setTitle("Maths").setDescription(`• a.calc
-   `);
-
-   
-   const help4 = new Discord.MessageEmbed().setColor("GREEN").setAuthor('Raju Commands').setTitle("Weeb").setDescription(`• a.anime
-
-   • a.hentai
-   
-   • a.neko
-    
-   • a.smug
-   
-   • a.baka
-
-   • a.tickle
-   
-   • a.slap
-
-   • a.poke
-   
-   • a.nekoGif
-   
-   • a.kiss
-
-   • a.hug
-   
-   • a.poke
-   
-   • a.nekoGif
-   
-   • a.cuddle
-
-   • a.kemonomimi
-   
-   • a.wallpaper
-
-   • a.fact
-
-   
-   `);
-   
-   
-   const help5 = new Discord.MessageEmbed().setColor("GREEN").setAuthor('Raju Commands').setTitle("Animals").setDescription(`• a.dog
-
-   • a.cat
-   `);
-   
-   
-
-   pages = [
-    help1,
-    help2,
-    help3,
-    help4,
-    help5
-];
-
-
-paginationEmbed(message, pages,['⏪', '⏩'],  92000000);
+  message.channel.send(help);
 }
 
 module.exports.help = {
     name:"help",
     aliases: []
-}
+} 
