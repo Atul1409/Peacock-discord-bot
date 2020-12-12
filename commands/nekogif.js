3afee1d0c9df2473fc56eded579c8882fdf2de2b
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-12 22:28:54 +0530

    Update nekogif.js

--- commands/nekogif.js
+++ commands/nekogif.js
@@ -10,6 +10,7 @@ exports.run = async (bot , message, args) => {
         .setTitle(`nya!`)
 		.setImage(img)
 		.setColor('RANDOM')
+	 .setFooter('Powered by nekos.life')
 
 	message.channel.send(embed)
 }
