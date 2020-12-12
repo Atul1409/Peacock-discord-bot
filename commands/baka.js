094ffaabb40a97504d061d66e98620e84339f034
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-12 22:25:33 +0530

    Update baka.js

--- commands/baka.js
+++ commands/baka.js
@@ -10,6 +10,7 @@ exports.run = async (bot , message, args) => {
         .setTitle(`baka!`)
 		.setImage(img)
 		.setColor('RANDOM')
+	        .setFooter('Powered by nekos.life')
 
 	message.channel.send(embed)
 }
