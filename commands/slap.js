b8ad713e82ab08f69b4ce4a61ac480c4bb14606d
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-18 00:02:40 +0530

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -3,7 +3,8 @@ const neko_client = require('nekos.life')
 const neko = new neko_client()
 let img, slap , embed
 exports.run = async (bot , message, args) => {
-    const user = message.mentions.users.first() || message.author ;
+ const user = message.mentions.users.first() ;
+    if(!message.mentions.users.first()) return message.channel.send("pls mention someone")
 	slap = await  neko.sfw.slap()
 	img = slap.url
     embed = new Discord.MessageEmbed()
