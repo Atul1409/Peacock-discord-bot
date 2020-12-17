e2e7b13c10fcf049661cabe543e3c97b50409486
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-17 23:59:15 +0530

    Update cuddle.js

--- commands/cuddle.js
+++ commands/cuddle.js
@@ -3,7 +3,8 @@ const neko_client = require('nekos.life')
 const neko = new neko_client()
 let img, cuddle , embed
 exports.run = async (bot , message, args) => {
-    const user = message.mentions.users.first() || message.author ;
+     const user = message.mentions.users.first() ;
+    if(!message.mentions.users.first()) return message.channel.send("pls mention someone")
 	cuddle = await  neko.sfw.cuddle()
 	img = cuddle.url
     embed = new Discord.MessageEmbed()
