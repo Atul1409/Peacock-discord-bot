56fcf0c9a20567ffec473ff2bc5330f759fd5a01
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-18 00:01:45 +0530

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -3,7 +3,8 @@ const neko_client = require('nekos.life')
 const neko = new neko_client()
 let img, pat , embed
 exports.run = async (bot , message, args) => {
-    const user = message.mentions.users.first() || message.author ;
+    const user = message.mentions.users.first() ;
+    if(!message.mentions.users.first()) return message.channel.send("pls mention someone")
 	pat = await  neko.sfw.pat()
 	img = pat.url
     embed = new Discord.MessageEmbed()
