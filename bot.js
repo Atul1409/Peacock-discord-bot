dd7d68c1048e70f3e65d61597b4c08a8277c3f12
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2021-02-10 16:12:02 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ fs.readdir("./commands/",(err,files) => {
 
 bot.on("ready",async() => {
     console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`)
-    bot.user.setActivity(`Playing in ${bot.guilds.cache.size} servers!`, { type: 'STREAMING' });
+    bot.user.setActivity(`in ${bot.guilds.cache.size} servers!`, { type: 'COMPETING' });
 })
 
 bot.on("message",async message =>{
