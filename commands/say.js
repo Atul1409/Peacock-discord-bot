d40aebb7a260c0ba89505ecb20277d35ebe0c487
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2021-02-24 14:30:10 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 require('dotenv').config();
 
 module.exports.run = async(bot, message , args) => {
-  if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("I can't :(")
+ /* if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("I can't :(")*/
   if(message.author.id != process.env.OWNER) return message.reply("I can't :(")
   let botmessage = args.join(' ');
   message.delete().catch();
